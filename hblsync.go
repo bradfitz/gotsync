47987443345d7428ccaa44d1e81704e072a8b483
Brad Fitzpatrick <brad@danga.com>
2010-05-01 18:17:26 -0700

    fast RemoveAll support, and helpers

--- hblsync.go
+++ hblsync.go
@@ -8,9 +8,43 @@
 
 package main
 
+import "container/vector"
+import "exp/iterable"
 import "fmt"
 import "flag"
 import "os"
+
+var delete *bool = flag.Bool("delete", false, "delete mode (takes 1 argument)")
+
+func usage() {
+	os.Stderr.WriteString("Usage: hblsync <src_dir> <dst_dir>\n")
+	os.Stderr.WriteString("       hblsync --delete <dir>\n")
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+	resultChan := make(chan SyncStats)
+
+	// Delete mode
+	if (*delete) {
+		if (flag.NArg() != 1) { usage() }
+		go RemoveAll(flag.Arg(0), resultChan)
+		fmt.Println("Results:", <-resultChan)
+		return
+	}
+
+	// Sync mode
+	if (flag.NArg() != 2) {
+		usage();
+	}
+	srcFile := openDirectoryOrDie("source", flag.Arg(0))
+	dstFile := openDirectoryOrDie("destination", flag.Arg(1))
+
+	go SyncDirectories(srcFile, dstFile, resultChan)
+	fmt.Println("Results:", <-resultChan)
+}
+
 
 func openDirectoryOrDie(which string, dirName string) *os.File {
 	file, e := os.Open(dirName, os.O_RDONLY, 0)
@@ -35,13 +69,46 @@ func openDirectoryOrDie(which string, dirName string) *os.File {
 
 type SyncStats struct {
 	ErrorCount int
+
+	FilesGood int   // no change needed
+	DirsGood int    // already existed (contents might be wrong)
+
 	DirsCreated int
-	DirsDeleted int
-	DirsGood int
 	FilesCreated int
-	FilesReplaced int
+
+	FilesReplaced int  // existed, but wrong. doesn't count in FilesDeleted
+	DirsDeleted int
 	FilesDeleted int
-	FilesGood int
+}
+
+func (stats *SyncStats) incrementBy(delta *SyncStats) {
+	stats.ErrorCount += delta.ErrorCount
+	stats.DirsCreated += delta.DirsCreated
+	stats.DirsDeleted += delta.DirsDeleted
+	stats.DirsGood += delta.DirsGood
+	stats.FilesCreated += delta.FilesCreated
+	stats.FilesReplaced += delta.FilesReplaced
+	stats.FilesDeleted += delta.FilesDeleted
+	stats.FilesGood += delta.FilesGood
+}
+
+type outstandingOps struct {
+	v vector.Vector;
+}
+
+func (ops *outstandingOps) new() chan SyncStats {
+	ch := make(chan SyncStats)
+	ops.v.Push(ch)
+	return ch
+}
+
+// Wait for all outstandin operations, summing the total into outStats
+func (ops *outstandingOps) wait(outStats *SyncStats) {
+	for _, value := range ops.v {
+		ch := value.(chan SyncStats)
+		subStats := <-ch
+		outStats.incrementBy(&subStats)
+	}
 }
 
 func readDirnames(dir *os.File, outNames *[]string, ok chan bool) {
@@ -52,6 +119,84 @@ func readDirnames(dir *os.File, outNames *[]string, ok chan bool) {
 	} else {
 		*outNames = names
 		ok <- true
+	}
+}
+
+func stringSet(items []string) map[string]bool {
+	var set = map[string]bool {}
+	for _, name := range items {
+		set[name] = true
+	}
+	return set
+}
+
+func mapLookup(someMap map[string]bool) func(entry interface{}) bool {
+	return func(entry interface{}) bool {
+                name := entry.(string)
+		if someMap[name] {
+			return true
+		}
+		return false
+	}
+}
+
+func RemoveAll(filename string, out chan SyncStats) {
+	stats := new(SyncStats)
+	defer func() {
+		out <- *stats
+	}()
+	defer func() {
+		fmt.Println("RemoveAll:", filename, stats)
+	}()
+
+	dirstat, err := os.Lstat(filename)
+	if err != nil {
+		stats.ErrorCount++
+		return
+	}
+
+	// Leaf case: if Remove works, we're done.
+	err = os.Remove(filename)
+	if err == nil {
+		if dirstat.IsDirectory() {
+			stats.DirsDeleted++
+		} else {
+			stats.FilesDeleted++
+		}
+		return
+	}
+
+	// Otherwise, is this a directory we need to recurse into?
+	if !dirstat.IsDirectory() {
+		stats.ErrorCount++
+                return
+	}
+
+	fd, err := os.Open(filename, os.O_RDONLY, 0)
+	if err != nil {
+		stats.ErrorCount++
+                return
+	}
+	defer fd.Close()
+
+	names, err := fd.Readdirnames(-1)
+	if err != nil {
+		stats.ErrorCount++
+                return
+	}
+
+	ops := new(outstandingOps)
+	for _, name := range names {
+		go RemoveAll(fmt.Sprintf("%s/%s", filename, name), ops.new())
+	}
+	ops.wait(stats)
+
+	// Delete the final directory
+	err = os.Remove(filename)
+	if err != nil {
+		stats.ErrorCount++
+	} else {
+		stats.DirsDeleted++
 	}
 }
 
@@ -76,26 +221,42 @@ func SyncDirectories(srcDir *os.File, dstDir *os.File, out chan SyncStats) {
 		return
 	}
 
+	srcSet := stringSet(srcDirnames)
+	dstSet := stringSet(dstDirnames)
+	inSourceDir := mapLookup(srcSet)
+	inDestDir := mapLookup(dstSet)
+	notInSourceDir := func(entry interface{}) bool {
+		return !inSourceDir(entry)
+	}
+
+	srcNamesIter := iterable.StringArray(srcDirnames)
+	dstNamesIter := iterable.StringArray(dstDirnames)
+	inDst, notInDst := iterable.Partition(srcNamesIter, inDestDir)
+	toBeDeletedNames := iterable.Filter(dstNamesIter, notInSourceDir)
+
+	ops := new(vector.Vector)
+
 	// TODO: sync contents
 	fmt.Println("src contents:", srcDirnames)
 	fmt.Println("dst contents:", dstDirnames)
 
+	fmt.Println("inDst contents:")
+	for e := range inDst.Iter() {
+		fmt.Println("  *", e)
+	}
+
+	fmt.Println("notInDst contents:")
+	for e := range notInDst.Iter() {
+		fmt.Println("  *", e)
+	}
+
+	fmt.Println("To be deleted:")
+	for e := range toBeDeletedNames.Iter() {
+		fmt.Println("  * (delete)", e)
+		ch := make(chan SyncStats)
+		go RemoveAll(fmt.Sprintf("%s/%s", dstDir.Name(), e), ch)
+		ops.Push(ch)
+	}
+	
 	out <- *stats
 }
-
-func main() {
-	flag.Parse()
-	if (flag.NArg() != 2) {
-		os.Stderr.WriteString("Usage: hblsync <src_dir> <dst_dir>\n")
-		os.Exit(1)
-	}
-	srcFile := openDirectoryOrDie("source", flag.Arg(0))
-	dstFile := openDirectoryOrDie("destination", flag.Arg(1))
-
-	ch := make(chan SyncStats)
-	go SyncDirectories(srcFile, dstFile, ch)
-	results := <-ch
-
-	fmt.Println("Results:", results)
-}
-
